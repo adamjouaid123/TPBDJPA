e5f088cc717c758f85d6b75f592550c4f2b0d85c
Adam Jouaid <adam.jouaid123@gmail.com>
2024-10-16 12:45:08 +0200

    book.java ok

--- src/main/java/org/tp/data/Book.java
+++ src/main/java/org/tp/data/Book.java
@@ -4,22 +4,24 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-// ToDo: Définir Book comme source d'héritage en mode SINGLE_TABLE
-
-// ToDo: Définir l'attribut discriminant pour identifier les sous-classes
-
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "book_type", discriminatorType = DiscriminatorType.STRING)
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
 
-    // ToDo: Définir le type de lien avec Category
-    
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
-    
-    // ToDo: Définir le type de lien avec Author (On ne définit pas le lien inverse ici)
+    @ManyToMany
+    @JoinTable(
+        name = "book_author",
+        joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
     private Set<Author> authors;
 
     // Getters and Setters
